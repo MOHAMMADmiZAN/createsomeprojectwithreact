2dcf90861b82e5deb83e53e5e0146c8a713a578e
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-08-28 11:10:39 +0600

    hex update

--- src/pages/CoronaUpdate/CoronaUpdate.jsx
+++ src/pages/CoronaUpdate/CoronaUpdate.jsx
@@ -36,6 +36,8 @@ function CoronaUpdate() {
                     deathsCount: response.data[0].deaths,
                 }
             })
+
+        }).then(() => {
             console.log(covidData);
         }).catch(function (error) {
             console.error(error);
@@ -43,47 +45,60 @@ function CoronaUpdate() {
     }
     useEffect(() => {
         covid19()
-    },[])
+    }, [])
 
     return (
         <>
-        <section className={`${styles.covidSection}`} >
-        <div className={`${styles.covidOverlay}`}>
-            <Container>
-                <Row className={"text-center"}>
-                    <Col lg={12}>
-                        <Card>
-                            <Card.Header as={'h3'} className={styles.cardCustom}> Covid 19 Update</Card.Header>
-                            <Card.Body className={styles.cardBodyCustom}>
-                                <Table striped bordered hover variant="danger">
-                                    <thead>
-                                    <tr>
-                                        <th>Country</th>
-                                        <th>ConfirmedCase</th>
-                                        <th>RecoveredCase</th>
-                                        <th>CriticalCase</th>
-                                        <th>Deaths</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>{covidData.status.countryName}</td>
-                                        <td>{covidData.status.confirmedCount}</td>
-                                        <td>{covidData.status.recoveredCount}</td>
-                                        <td>{covidData.status.criticalCount}</td>
-                                        <td>{covidData.status.deathsCount}</td>
-                                    </tr>
-                                    </tbody>
+            <section className={`${styles.covidSection}`}>
+                <div className={`${styles.covidOverlay}`} style={{padding: "30px"}}>
+                    <Container>
+                        <Row className={"text-center"}>
+                            <Col lg={12}>
+                                <Card className={styles.cardCustom}>
+                                    <Card.Header as={'h3'} className={styles.cardHeaderCustom}> Covid 19
+                                        Update</Card.Header>
+                                    <Card.Body className={styles.cardBodyCustom}>
+                                        <Table striped bordered hover>
+                                            <thead>
+                                            <tr>
+                                                <th style={{background: "#363636", color: "white"}}>Country</th>
+                                                <th style={{background: "orange", color: "white"}}>ConfirmedCase</th>
+                                                <th style={{background: "green", color: "white"}}>RecoveredCase</th>
+                                                <th style={{background: "orange", color: "white"}}>CriticalCase</th>
+                                                <th style={{background: "red", color: "white"}}>Deaths</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td style={{background: "rgba(54,54,54,0.2)"}}>{covidData.status.countryName}</td>
+                                                <td style={{
+                                                    color: "orange",
+                                                    background: "rgba(255,165,0,0.2)"
+                                                }}>{covidData.status.confirmedCount}</td>
+                                                <td style={{
+                                                    color: "green",
+                                                    background: "rgba(0,128,0,0.2)"
+                                                }}>{covidData.status.recoveredCount}</td>
+                                                <td style={{
+                                                    color: "orange",
+                                                    background: "rgba(255,165,0,0.2)"
+                                                }}>{covidData.status.criticalCount}</td>
+                                                <td style={{
+                                                    color: "red",
+                                                    background: "rgba(255,0,0,0.2)"
+                                                }}>{covidData.status.deathsCount}</td>
+                                            </tr>
+                                            </tbody>
 
-                                </Table>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+                                        </Table>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
 
-        </div>
-        </section>
+                </div>
+            </section>
         </>
     );
 }
