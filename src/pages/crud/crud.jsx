4b8cad8b23a62e9bdbb0aeaee352cdaa67e739bd
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-09-06 17:53:30 +0600

    Crud update

--- src/pages/crud/crud.jsx
+++ src/pages/crud/crud.jsx
@@ -1,92 +1,169 @@
-import React, {useEffect} from 'react';
-import {Button, Card, Col, Container, Form, Row, Table} from "react-bootstrap";
+import React, {useEffect, useState} from 'react';
+import {Button, Card, Col, Container, Form, Modal, Row, Table} from "react-bootstrap";
 import styles from "../../assets/styles/global.module.css"
-import data from "bootstrap/js/src/dom/data";
+
 
 function Crud() {
-    // const [studentData,setStudentData] = useState([
-    //     {
-    //         name: "",
-    //         roll: 0,
-    //         department: "",
-    //         section: "",
-    //     }
-    // ])
+    const [data, setData] = useState([])
+    const [lgShow, setLgShow] = useState(false);
+
+    const [studentData, setStudentData] = useState({
+        name: "",
+        roll: 0,
+        department: "",
+        section: "",
+    })
+    const handleClick = (e) => {
+        setStudentData(
+            {
+                ...studentData,
+                [e.target.name]: e.target.value
+            }
+        )
+
+    }
     useEffect(() => {
         document.title = "Crud"
     })
-    const fetchStudent = () => {
-        fetch('http://127.0.0.1:8000/api/students')
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(err => console.log(err))
+    const fetchStudent = async () => {
+        let url = 'http://127.0.0.1:8000/api/students'
+        try {
+            const response = await fetch(url)
+            let data = await response.json();
+            setData(data)
+            console.log(data)
+
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }
+    const deletedStudent = async (studentId) => {
+        let deleteUrl = `http://127.0.0.1:8000/api/studentsRemove/${studentId}`
+        try {
+            const deleteResponse = await fetch(deleteUrl)
+            let deleteData = await deleteResponse.json();
+            console.log(deleteData);
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const insertStudent = async (e) => {
+        e.preventDefault()
+        let insertStudentUrl = 'http://127.0.0.1:8000/api/studentInsert'
+        try {
+            const insertResponse = await fetch(insertStudentUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(studentData)
+            });
+            return insertResponse.json()
+        } catch (e) {
+            console.log(e)
+        }
+
+        console.log(...studentData)
     }
     useEffect(() => {
         fetchStudent()
+
     }, [])
     return (
         <>
             <Container>
                 <Row>
-                    <Col lg={8}>
+                    <Col lg={9}>
                         <Card className={styles.cardCustom}>
                             <Card.Header className={styles.cardHeaderCustom} as={"h4"}>
                                 STUDENT DATA VIEW
                             </Card.Header>
                             <Card.Body className={styles.cardBodyCustom}>
-                              <Table striped bordered hover variant={"info"}>
-                                  <thead>
-                                  <tr>
-                                      <th>SL</th>
-                                      <th>NAME</th>
-                                      <th>ROLL</th>
-                                      <th>DEPARTMENT</th>
-                                      <th>Section</th>
-                                      <th>Action</th>
-                                  </tr>
-                                  </thead>
-                                  <tbody>
+                                <Table striped bordered hover variant={"primary"}>
+                                    <thead>
+                                    <tr>
+                                        <th>SL</th>
+                                        <th>NAME</th>
+                                        <th>ROLL</th>
+                                        <th>DEPARTMENT</th>
+                                        <th>Section</th>
+                                        <th>Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {
+                                        data.map((d, i) => {
 
-                                  </tbody>
+                                            return <tr key={Math.floor(Math.random() * 10000).toString()}>
+                                                <td>{i + 1}</td>
+                                                <td>{d.name}</td>
+                                                <td>{d.roll}</td>
+                                                <td>{d.department}</td>
+                                                <td>{d.section}</td>
+                                                <td><Button variant={"info"} className={`mx-1`}
+                                                            onClick={() => setLgShow(true)}>Update</Button>
+                                                    <Button variant={"danger"} className={`mx-1`}
+                                                            onClick={() => deletedStudent(d.id)}>Delete</Button>
+                                                </td>
+                                            </tr>
+                                        })
+                                    }
+                                    </tbody>
 
-                              </Table>
+                                </Table>
                             </Card.Body>
 
                         </Card>
                     </Col>
-                    <Col lg={4}>
+                    <Col lg={3}>
                         <Card className={styles.cardCustom}>
                             <Card.Header className={styles.cardHeaderCustom} as={"h4"}>
                                 STUDENT DATA INSERT
                             </Card.Header>
                             <Card.Body className={styles.cardBodyCustom}>
-                                <Form>
+                                <Form onSubmit={insertStudent}>
                                     <Form.Group controlId={"studentName"}>
                                         <Form.Label className={styles.paddingLabel10}> Student Name:</Form.Label>
                                         <Form.Control type={'text'} placeholder={'Type Student Name'}
-                                                      name={"name"}>
+                                                      name={"name"} onChange={handleClick}>
                                         </Form.Control>
                                     </Form.Group>
                                     <Form.Group controlId={"studentRoll"}>
                                         <Form.Label className={styles.paddingLabel10}> Student Roll:</Form.Label>
                                         <Form.Control type={'text'} placeholder={'Type Student Roll'}
-                                                      name={"roll"}>
+                                                      name={"roll"} onChange={handleClick}>
                                         </Form.Control>
                                     </Form.Group>
                                     <Form.Group controlId={"studentDepartment"}>
                                         <Form.Label className={styles.paddingLabel10}> Student Department:</Form.Label>
                                         <Form.Control type={'text'} placeholder={'Type Student Department Name'}
-                                                      name={"department"}>
+                                                      name={"department"} onChange={handleClick}>
                                         </Form.Control>
                                     </Form.Group>
                                     <Form.Group controlId={"studentSection"}>
                                         <Form.Label className={styles.paddingLabel10}> Student Section:</Form.Label>
                                         <Form.Control type={'text'} placeholder={'Type Student Section'}
-                                                      name={"section"}>
+                                                      name={"section"} onChange={handleClick}>
                                         </Form.Control>
                                     </Form.Group>
-                                    <Button variant={"info"} style={{marginTop: "10px"}}>INSERT</Button>
+                                    <Button variant={"info"} style={{marginTop: "10px"}} type={"submit"}>INSERT</Button>
                                 </Form>
+                                <Modal
+                                    size="lg"
+                                    show={lgShow}
+                                    onHide={() => setLgShow(false)}
+                                    aria-labelledby="example-modal-sizes-title-lg"
+                                >
+                                    <Modal.Header closeButton>
+                                        <Modal.Title id="example-modal-sizes-title-lg">
+                                            Large Modal
+                                        </Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>...</Modal.Body>
+                                </Modal>
                             </Card.Body>
 
                         </Card>
